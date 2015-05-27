869ba0f1f008afe0939ea921d1da8f39609d9874
Bruno Kaiser <bruno@bkaiser.ch>
2015-05-27 23:50:37 +0200

    remove some unused dependencies

--- src/java/test/org/opentdc/wtt/SubProjectTest.java
+++ src/java/test/org/opentdc/wtt/SubProjectTest.java
@@ -5,10 +5,8 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 
 import javax.ws.rs.core.MediaType;
